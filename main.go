623dddd7c1ba0fdd5a03c28716329f0b54d67111
hezhihao <zhihaohe0906@gmail.com>
2024-05-18 22:26:12 +0800

    更新

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+		c.JSON(200, gin.H{"message": "successful"})
 	})
 	r.Run(":8081")
 }
